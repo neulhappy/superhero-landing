1d5adbce4f19ac99cad8fc1a40d102760ade9dcd
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2023-12-11 11:15:07 +0900

    [Style] InputInterface 클래스 이름 변경

--- core/src/main/java/org/util/Interface.java
+++ core/src/main/java/org/util/Interface.java
@@ -3,7 +3,7 @@ package org.util;
 import java.util.Arrays;
 
 //테스터 클래스 활용을 위한 임시 인터페이스
-public class InputInterface {
+public class Interface {
 
 
     public static int testIntInput() {
@@ -28,4 +28,12 @@ public class InputInterface {
             System.out.println("testArrayInput malfunction. value : " + Arrays.toString(inp));
         return inp;
     }
+
+
+    public static void testStiringOutput(String... lines) {
+        Tester.stringOutput(lines);
+    }
+    //가변 매개변수는 String 배열이나 s1, s2같은 식으로 여러개의 매개변수를 원하는 숫자만큼 전달할 수 있어요.
+    //ex) testStringOutput(s1, s2, s3, s4}
+    //ex2) String[] lines = {s1, s2, s3, s4}; testStringOutput(lines);
 }
