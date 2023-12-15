d7dfd43eb32c6c3b8c84cf8796d0542d704d0990
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2023-12-15 15:58:23 +0900

    [feat] BoardDAO 구현, QuestionController doPost 구현

--- web/src/main/java/com/board/QestionController.java
+++ web/src/main/java/com/board/QestionController.java
@@ -1,8 +1,34 @@
 package com.board;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet
 public class QestionController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String saveDir = req.getServletContext().getRealPath("/upload");
+
+        BoardDTO dto = new BoardDTO();
+        dto.setTitle(req.getParameter("title"));
+        dto.setContent(req.getParameter("content"));
+        dto.setAuthor_id(Integer.parseInt(req.getParameter("UserId")));
+
+
+        BoardDAO dao = new BoardDAO();
+        int result = dao.insertWrite(dto);
+        dao.close();
+
+        if (result == 1) {
+            resp.sendRedirect("../mvcboard/list.do");
+        } else {
+            //실패
+        }
+    }
 }
