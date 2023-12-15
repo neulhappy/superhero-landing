fd274ce4996e71aea7a9c530387a3167f47fcfd0
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2023-12-15 11:47:23 +0900

    [feat] PreparedStatement 동적 스트링 할당 지원 메소드 추가.

--- web/src/main/java/com/util/DBConnPool.java
+++ web/src/main/java/com/util/DBConnPool.java
@@ -1,12 +1,10 @@
 package com.util;
 //
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DBConnPool {
     public Connection con;
@@ -14,30 +12,37 @@ public class DBConnPool {
     public PreparedStatement psmt;
     public ResultSet rs;
 
-    public DBConnPool(){
-        try{
+    public DBConnPool() {
+        try {
             Context initCtx = new InitialContext();
-            Context ctx = (Context)initCtx.lookup("java:comp/env");
+            Context ctx = (Context) initCtx.lookup("java:comp/env");
             DataSource source = (DataSource) ctx.lookup("dbcp-myoracle");
 
             con = source.getConnection();
             Logger.logger.info("DB 커넥션 풀 연결 성공");
-        }catch (Exception e){
+        } catch (Exception e) {
             Logger.logger.error("DBConnPool 시도 중 예외 발생");
         }
     }
 
-    public void close(){
-        try{
-            if(rs !=null) rs.close();
-            if(stmt !=null) stmt.close();
-            if(psmt !=null) psmt.close();
-            if(con !=null) con.close();
+    public void close() {
+        try {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (psmt != null) psmt.close();
+            if (con != null) con.close();
 
             Logger.logger.info("DB 커넥션 풀 자원 반납");
-        }catch (Exception e){
+        } catch (Exception e) {
             Logger.logger.error("DB 커넥션 풀 자원 반납");
         }
     }
 
+    public static PreparedStatement setString(PreparedStatement psmt, String... string) throws SQLException {
+        int parameterIndex = 1;
+        for (String str : string) {
+            psmt.setString(parameterIndex, str);
+        }
+        return psmt;
+    }
 }
