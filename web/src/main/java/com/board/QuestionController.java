3a01ccae1b960ec42653d6e3a2cc4409a54bcef2
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2023-12-18 09:18:23 +0900

    [fix] typo

--- web/src/main/java/com/board/QuestionController.java
+++ web/src/main/java/com/board/QuestionController.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet
-public class QestionController extends HttpServlet {
+public class QuestionController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
