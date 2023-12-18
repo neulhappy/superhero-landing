c6a9a79136b7ae33093bd2dba229c912ab41b3b7
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2023-12-18 13:22:10 +0900

    [feat] insertOrder 추가

---
+++ web/src/main/java/com/shop/OrderDAO.java
@@ -0,0 +1,43 @@
+package com.shop;
+
+import com.util.DBConnPool;
+import com.util.Logger;
+
+import java.sql.ResultSet;
+
+public class OrderDAO extends DBConnPool {
+
+    public int insertOrder(OrderDTO dto) {
+        int orderId = -1;
+        try {
+            //쿼리 작성
+            String query = "INSERT INTO order ( "
+                    + "custom_id, purchaser_name, recipient_name, address, contact)"
+                    + "VALUES( "
+                    + dto.getCustom_id() + "," + dto.getPurchaser_name() + "," + dto.getRecipient_name() + "," + dto.getAddress() + "," + dto.getContact() + ")";
+
+
+            psmt = con.prepareStatement(query, psmt.RETURN_GENERATED_KEYS);
+
+            int result = psmt.executeUpdate();
+            if (result > 0) {
+                try (ResultSet generatedKeys = psmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        orderId = generatedKeys.getInt(1);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Logger.error("insertWrite 메소드 오류 발생");
+        }
+
+        for (OrderDTO.ProductSet set : dto.getProductList()) {
+            String query = "INSERT INTO order-product ( "
+                    + "order_id, prod_id, quantity)"
+                    + "VALUES( "
+                    + orderId + "," + set.getProd_id() + "," + set.getQuantity();
+        }
+
+        return orderId;
+    }
+}
--- web/src/main/java/com/shop/OrderDTO.java
+++ web/src/main/java/com/shop/OrderDTO.java
@@ -108,5 +108,21 @@ public class OrderDTO {
     class ProductSet {
         private int prod_id;
         private int quantity;
+
+        public int getProd_id() {
+            return prod_id;
+        }
+
+        public void setProd_id(int prod_id) {
+            this.prod_id = prod_id;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
     }
 }
