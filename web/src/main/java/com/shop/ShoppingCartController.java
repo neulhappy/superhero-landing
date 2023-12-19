febe75c764e86daf3e63b968e481ae5b9e4bfe16
yeo-ok <wls990728@naver.com>
2023-12-19 17:19:59 +0900

    [feat] ShoppingCartController.java 추가

--- web/src/main/java/com/shop/ShoppingCartController.java
+++ web/src/main/java/com/shop/ShoppingCartController.java
@@ -1,4 +1,22 @@
 package com.shop;
 
-public class ShoppingCartController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/mypage/MyShoppingCart.do")
+public class ShoppingCartController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductDAO dao = new ProductDAO();
+        ProductDTO dto = dao.selectProduct(req.getParameter("id"));
+        HttpSession session = req.getSession();
+        session.setAttribute("productDTO", dto);
+    }
 }
