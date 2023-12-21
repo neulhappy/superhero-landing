e0a279f5d29e14a5e98dafd211b5abaefc0bcd33
yeo-ok <wls990728@naver.com>
2023-12-21 15:37:51 +0900

    [feat] OrderController(PID값을 로그인으로) 수정 order.js 수정 (실패시 alert)

--- web/src/main/java/com/shop/OrderController.java
+++ web/src/main/java/com/shop/OrderController.java
@@ -1,5 +1,7 @@
 package com.shop;
 
+import com.member.MemberDAO;
+import com.util.Alert;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,20 +10,25 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
-@WebServlet("/shop/order.do")
+@WebServlet("/shop/orderSuccess.do")
 public class OrderController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("order/order.jsp").forward(req, resp);
+        req.getRequestDispatcher("shop/order.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        int PID = (int) session.getAttribute("userPID");
-
+        MemberDAO dao = new MemberDAO();
+        int PID = dao.login((String) session.getAttribute("userId"), (String) session.getAttribute("userPw"));
+        if (PID < 1){
+            PrintWriter out = resp.getWriter();
+            Alert.alertLocation("잘못된 접근입니다.", "/index.jsp",out);
+        }else {
         ArrayList<OrderDTO.ProductSet> products = new ArrayList();
 
 
@@ -32,10 +39,10 @@ public class OrderController extends HttpServlet {
         dto.setAddress(req.getParameter("address"));
         dto.setContact(req.getParameter("contact"));
         dto.setProductList(products);
-        OrderDAO dao = new OrderDAO();
-        int orderId = dao.insertOrder(dto);
+        OrderDAO oDao = new OrderDAO();
+        int orderId = oDao.insertOrder(dto);
         System.out.println("insertQuery작동됨");
-        dao.close();
+        oDao.close();
 
         // 그 밖에 결제 과정에서 필요한 요소들
 
@@ -46,4 +53,4 @@ public class OrderController extends HttpServlet {
             //order 생성 실패
         }
     }
-}
+}}
